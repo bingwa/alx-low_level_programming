30ffd59724cf9cd663491b0c367f6f786206e7d4
bingwa <munyaobryan@gmail.com>
2022-03-15 06:41:47 -0700

    low level programming

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,10 +9,14 @@
 int print_last_digit(int n)
 {
 
-	n = n >= 0 ? n : n * -1;
-	int last = n % 10;
-	_putchar('0' + last);
+	int nv;
 
-	return (last);
+	if (n < 0)
+		nv = -1 * (n % 10);
+	else
+		nv = n % 10;
+
+	_putchar(nv + '0');
+	retrun (nv);
 
 }
