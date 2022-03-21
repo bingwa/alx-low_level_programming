b5150705defcec9d74707109fcde487aad62c3c4
bingwa <munyaobryan@gmail.com>
2022-03-21 10:44:20 -0700

    low level programming

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - print n elements of an array of integers
+ * followed by new line
+ * @a: Array
+ * @n: Number of array
+ * Return: Always 0 (Success)
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; n > 1; i++)
+		{
+			if (i != n - 1)
+			{
+				printf("%d, ", a[i]);
+			}
+			else
+			{
+				printf("%d", a[i]);
+			}
+		}
+	}
+	printf('\n');
+}
