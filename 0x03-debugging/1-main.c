3bb16540e74da5d8dad14dea171b4df031914c41
bingwa <munyaobryan@gmail.com>
2022-03-22 03:39:10 -0700

    low level programming

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - causes an infinite loop
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinte loop incoming :(\n)";
+
+	/**
+	 * i = 0
+	 * while (i < 10)
+	 * {
+	 * 	putchar(i);
+	 * }
+	 */
+	
+	printf("Infinite loop avoided! \\o/\n")
+	return (0);
+}
