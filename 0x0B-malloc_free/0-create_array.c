2e733e53da092786ea25209ef1651d9f8d5a5a9c
bingwa <munyaobryan@gmail.com>
2022-04-04 22:27:23 -0700

    low level programming

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 
-/** 
- * *create_array - creates an array of chars, and initializes it with a specific char.
+/**
+ * *create_array - creates an array of chars,
+ * and initializes it with a specific char.
  * @size: size of the array
  * @c: char to initialize
  * Return: pointer to the array initialized or NULL
@@ -10,6 +11,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *m = malloc(size);
+
 	if (size == 0 || m == 0)
 		return (0);
 
