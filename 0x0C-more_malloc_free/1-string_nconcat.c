e4efd0fb5ac76bce04c1d6f7bebc53cffaafa735
bingwa <munyaobryan@gmail.com>
2022-04-07 04:14:13 -0700

    low level programming

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,16 +1,53 @@
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * *malloc_checked - allocates memory using malloc and exit if failed
- * @b: int
- * Return: pointer to the array initialized or NULL
+ * _strlen - find length of a string
+ * @s: string
+ * Return: int
  */
 
-void *malloc_checked(unsigned int b)
+unsigned int _strlen(char *s)
 {
-int *m = malloc(b);
+unsigned int size = 0;
+for (; s[size] != '\0'; size++)
+;
+return (size);
+}
+
+/**
+ * *string_nconcat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: first bytes of s2 to be used
+ * Return: pointer or NULL
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int i, j;
+char *m;
+
+if (s1 == NULL)
+	s1 = "";
+if (s2 == NULL)
+	s2 = "";
+
+if (n < _strlen(s2))
+	m = malloc(_strlen(s1) + n * sizeof(char) + 1);
+else
+	m = malloc(_strlen(s1) + _strlen(s2) + 1);
+
 if (m == 0)
-	exit(98);
+	return (NULL);
+
+for (i = 0; s1[i] != '\0'; i++)
+	m[i] = s1[i];
+
+for (j = 0; s2[j] != '\0' && j < n; i++, j++)
+	m[i] = s2[j];
+
+m[i] = '\0';
 
 return (m);
 }
+
